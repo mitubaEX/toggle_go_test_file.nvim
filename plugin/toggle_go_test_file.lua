72d3a31f3e0f62f5f72c45a263a0453a9d79157b
mitubaEX <jun-nakamura@c-fo.com>
2024-03-16 11:26:20 +0900

    remove option

--- plugin/toggle_go_test_file.lua
+++ plugin/toggle_go_test_file.lua
@@ -1,8 +1,1 @@
-vim.api.nvim_create_user_command('ToggleGoTestFile', 'lua require("toggle_go_test_file").toggle_go_test_file()', {
-  nargs = 0,
-  complete = 'file',
-  bang = false,
-  bar = false,
-  range = '',
-  count = 0,
-})
+vim.api.nvim_create_user_command('ToggleGoTestFile', 'lua require("toggle_go_test_file").toggle_go_test_file()', {})
