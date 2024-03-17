8b99680941dcc39fd0d867b73bcc24a61d02650f
mitubaEX <jun-nakamura@c-fo.com>
2024-03-17 21:02:45 +0900

    feature: add toggle_go_file function

--- lua/toggle_go_test_file.lua
+++ lua/toggle_go_test_file.lua
@@ -1,6 +1,6 @@
 local vim = vim
 
-local function toggle_go_test_file()
+local function _toggle_go_test_file()
   -- main.go -> main_test.go
   local current_file_dir = vim.fn.expand('%:p:h')
   local current_file_name = vim.fn.expand('%:t:r')
@@ -18,6 +18,33 @@ local function toggle_go_test_file()
   end
 end
 
+local function _toggle_go_file()
+  -- main_test.go -> main.go
+  local current_file_dir = vim.fn.expand('%:p:h')
+  local current_file_name = vim.fn.expand('%:t:r')
+  local current_file_extension = vim.fn.expand('%:e')
+
+  local main_file_name = current_file_name:gsub('_test', '')
+  local main_file_path = current_file_dir .. '/' .. main_file_name .. '.' .. current_file_extension
+
+  if vim.fn.filereadable(main_file_path) == 1 then
+    vim.cmd('e ' .. main_file_path)
+  else
+    local main_file_content = [[package ]] .. main_file_name
+    vim.fn.writefile({main_file_content}, main_file_path)
+    vim.cmd('e ' .. main_file_path)
+  end
+end
+
+local function toggle_go_test_file()
+  local current_file_name = vim.fn.expand('%:t:r')
+  if current_file_name:find('_test') then
+    _toggle_go_file()
+  else
+    _toggle_go_test_file()
+  end
+end
+
 return {
-  toggle_go_test_file = toggle_go_test_file
+  toggle_go_test_file = toggle_go_test_file,
 }
